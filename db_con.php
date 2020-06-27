6c1ccdf904290872d56fd77e1b973436aea313b8
tuhinjamal <55732076+tuhinjamal@users.noreply.github.com>
2020-06-27 17:41:14 +0600

    Updated credentials for Heroku MySQL Database

--- db_con.php
+++ db_con.php
@@ -3,7 +3,7 @@
 $host_name='pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 $user_name='khmvvjvn2a6mvsql';
 $password='wkb21fytpf7lv0bt';
-$db_name='	mofliuhehoatrusq';
+$db_name='mofliuhehoatrusq';
 
    $connection=mysqli_connect($host_name,$user_name,$password);
    if ($connection) {

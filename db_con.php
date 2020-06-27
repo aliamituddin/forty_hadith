272a3028a2599b88f6a0cc5ff224249873741257
tuhinjamal <55732076+tuhinjamal@users.noreply.github.com>
2020-06-27 16:54:36 +0600

    Update db_con.php

--- db_con.php
+++ db_con.php
@@ -8,7 +8,7 @@ $db_name='db_hadith';
    $connection=mysqli_connect($host_name,$user_name,$password);
    if ($connection) {
 
-   		$db_select=mysqli_select_db($connection,$db_name,$db_name);
+   		$db_select=mysqli_select_db($connection,$db_name);
    				if ($db_select) {
    						//echo 'db selected';
    					# code...
